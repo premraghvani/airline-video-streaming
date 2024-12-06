192973520ac49c89e0bb8f5bc636dd4947852c0b
Prem Raghvani <69864865+premraghvani@users.noreply.github.com>
2024-12-06 12:45:56 +0000

    added video streaming element

--- commands/fetchFilmVideo.js
+++ commands/fetchFilmVideo.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+// inspiratoin: https://blog.logrocket.com/build-video-streaming-server-node/
+
 // retrieve video
 module.exports = {
     page: "/film/fetchvideo",
@@ -17,16 +19,35 @@ module.exports = {
             res.status(404).send("");
         }
 
-        // header range
+        // header range, needed to send the correct next parts
         const range = req.headers.range;
-        console.log(range)
+        if(!range){
+            res.status(400).send("Error: requires range headers")
+        }
         
         // retrieves video
         const pathToVideo = `./assets/videos/${id}.mp4`;
         const videoSize = fs.statSync(pathToVideo).size;
-        console.log(videoSize)
 
-        // sends a thumbnail back
-        res.send(fs.readFileSync(pathToVideo));
+        // chunks
+        const chunkSize = 0.5*10**6 // 500 KB chunk size
+        const start = Number(range.replace(/\D/g, ""));
+        const end = Math.min(start + chunkSize, videoSize - 1);
+
+        // breakpoint
+        if(start==end){return;}
+
+        // response headers
+        const headers = {
+            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": end-start,
+            "Content-Type": "video/mp4",
+        };
+        res.writeHead(206, headers);
+
+        // gets the 2MB max chunk and sends it
+        const videoStream = fs.createReadStream(pathToVideo, { start, end });
+        videoStream.pipe(res);
     }
 };
