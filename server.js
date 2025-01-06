a14d69c57763d3bacf4008a089f3295be00b8f70
Prem Raghvani <69864865+premraghvani@users.noreply.github.com>
2025-01-06 15:59:00 +0530

    ammended console messages and package.json

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const config = require("./config.json");
 const { app } = require("./app.js");
 
 app.listen(config.port);
+console.log(`Services available - access on this device at http://localhost:${config.port} (crew: http://localhost:${config.port}/crew) - please ensure ports are configured correctly should you want to access on other internet devices.`)
